a79180d74b084dc7229dddad76238499b2bc8dce
JohannesPutz <johannes.putz.2007@gmail.com>
2025-06-25 12:14:40 +0200

    Zweischenstand

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.ParticleSystemJobs;
+using TMPro;
 
 public class GameManagerScript : MonoBehaviour
 {
@@ -18,7 +19,45 @@ public class GameManagerScript : MonoBehaviour
     public Color _goalColour;
     public Color[] _colorsArray;
 
-    
+    [Header("RandomName")]
+    public GameObject randomNameCanvas;
+    public string randomName;
+    public TMP_Text randomNameText;
+
+    // Liste der männlichen Adjektive
+    private string[] adjektive = new string[]
+    {
+        "verrückter", "lustiger", "komischer", "schräger", "alberner", "zappeliger", "flippiger", "witziger", "seltsamer", "spaßiger",
+        "plumper", "lauter", "überdrehter", "durchgeknallter", "schriller", "cooler", "lässiger", "stylischer", "entspannter", "smarter",
+        "trendiger", "moderner", "souveräner", "chilliger", "lockerer", "selbstsicherer", "eleganter", "flinker", "glänzender", "wilder",
+        "starker", "mächtiger", "brutaler", "rasender", "donnernder", "explosiver", "brennender", "unaufhaltbarer", "tapferer", "mutiger",
+        "furchtloser", "heldenhafter", "krasser", "zäher", "unbesiegbarer", "robuster", "harter", "frostiger", "sonniger", "windiger",
+        "stürmischer", "erdiger", "felsiger", "nebliger", "feuriger", "wasserreicher", "dunkler", "gruseliger", "finsterer", "geisterhafter",
+        "spukhafter", "unheimlicher", "totenstiller", "schattenhafter", "kluger", "schlauer", "neugieriger", "cleverer", "tüftelnder",
+        "logischer", "grübelnder", "analytischer", "gelehrter", "nerdiger", "flauschiger", "zuckersüßer", "niedlicher", "funkelnder",
+        "kuscheliger", "fröhlicher", "glitzernder", "zarter", "hopsiger", "kulleriger", "bunter", "schimmernder", "magischer", "verträumter",
+        "freundlicher", "witzelnder", "geheimer", "mysteriöser", "unsichtbarer", "silberner", "goldener", "stahlharter", "verräterischer",
+        "leuchtender", "elektrischer", "mechanischer", "biestiger", "schlammiger", "kantiger", "schneller", "leiser", "aggressiver",
+        "geduldiger", "listiger", "gefährlicher", "selbstloser", "frecher", "verschrobener", "verwegener", "legendärer", "epischer",
+        "chaotischer", "genialer", "verpeilter", "nasser", "trockener", "blinder", "tauber", "wandelbarer", "fliegender", "tanzender",
+        "singender", "brüllender", "jagender", "zitternder", "schnarchender", "gähnender", "lachender", "weinender", "träumender"
+    };
+
+    // Liste der männlichen Tiere
+    private string[] tiere = new string[]
+    {
+        "Löwe", "Tiger", "Bär", "Wolf", "Fuchs", "Hirsch", "Eber", "Rabe", "Panther", "Adler",
+        "Falke", "Geier", "Stier", "Hund", "Kater", "Hahn", "Pfau", "Widder", "Ziegenbock", "Dachs",
+        "Marder", "Schakal", "Igel", "Hase", "Maulwurf", "Biber", "Otter", "Affe", "Gorilla", "Orang-Utan",
+        "Schimpanse", "Elefant", "Wal", "Delphin", "Hai", "Krake", "Fisch", "Pavian", "Yak", "Kojote",
+        "Büffel", "Zebra", "Nashorn", "Elch", "Mammut", "Drache", "Greif", "Minotaurus", "Zentaur", "Werwolf",
+        "Vogel", "Pinguin", "Strauß", "Kranich", "Schwan", "Spatz", "Specht", "Uhu", "Kauz", "Kondor",
+        "Luchs", "Wiesel", "Frettchen", "Kaninchen", "Kamel", "Esel", "Pony", "Ochse", "Rind", "Maultier",
+        "Frosch", "Kröterich", "Molch", "Leguan", "Iltis", "Käfer", "Skorpion", "Marienkäfer", "Schmetterling",
+        "Käuzchen", "Rüsselkäfer"
+    };
+
+
     void Start()
     {
         foreach (GameObject obj in objectsWithScripts)
@@ -37,15 +76,27 @@ public class GameManagerScript : MonoBehaviour
         }
     }
 
-    public void ActivateEverything()
+    public void ActivateRandomName()
     {
-        colourCanvas.SetActive(true);
-        GameObject caller = EventSystem.current.currentSelectedGameObject;
-        if (caller != null)
+        randomNameCanvas.SetActive(true);
+        GameObject callerx = EventSystem.current.currentSelectedGameObject;
+        if (callerx != null)
         {
-            caller.SetActive(false);
+            callerx.SetActive(false);
         }
 
+    }
+
+    public void ChooseRandomName()
+    {
+        randomName = adjektive[Random.Range(0, adjektive.Length)] + " " + tiere[Random.Range(0, tiere.Length)];
+        randomNameText.text = randomName;
+        StartCoroutine(WaitTime());
+    }
+    public void ActivateCoulorCanvas()
+    {
+        colourCanvas.SetActive(true);
+        
     }
 
     public void SelectColorGoal(int a)
@@ -72,6 +123,13 @@ public class GameManagerScript : MonoBehaviour
     }
     void Update()
     {
-        
+
+    }
+
+    public IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(2);
+        randomNameCanvas.SetActive(false);
+        ActivateCoulorCanvas();
     }
 }
